e5b8fc0964deb7aea9aa38ec3e934841d3ade7e7
Georgiy Tugai <georgiy.tugai@gmail.com>
2016-12-14 01:53:30 +1030

    Fix highlight match behaviour
    Documentation states that, in the 'highlight' field, both positive and negative matches terminate the search.
    This patch brings znc-push's behaviour in line with said documentation.

--- push.cpp
+++ push.cpp
@@ -869,9 +869,6 @@ class CPushMod : public CModule
 			options["highlight"].Split(" ", values, false);
 			values.push_back("%nick%");
 
-			bool matched = false;
-			bool negated = false;
-
 			for (VCString::iterator i = values.begin(); i != values.end(); i++)
 			{
 				CString value = i->AsLower();
@@ -902,18 +899,11 @@ class CPushMod : public CModule
 
 				if (msg.WildCmp(value))
 				{
-					if (negate_match)
-					{
-						negated = true;
-					}
-					else
-					{
-						matched = true;
-					}
+					return !negate_match;
 				}
 			}
 
-			return (matched && !negated);
+			return false;
 		}
 
 		/**
